dbb0d87275c19f9c968c91acf1d6b47869feae5c
alexandr <alexwork2704@yandex.ru>
2019-11-26 04:25:05 +0300

    поправил контенткую страницу - если она не главная, то показывает инфу

--- wp-content/themes/netromag/page.php
+++ wp-content/themes/netromag/page.php
@@ -6,28 +6,31 @@
  */
 
 get_header(); ?>
+	<?php 
+	//если это не главная страница, то показываем контент
+	if(is_home() || is_front_page() == false){ ?>
+		<section class="content-area">
+			<main id="main" class="site-main" role="main">
 
-	<section class="content-area">
-		<main id="main" class="site-main" role="main">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || '0' != get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+				<?php endwhile; // end of the loop. ?>
 
-			<?php endwhile; // end of the loop. ?>
-
-		</main>
-	</section>
+			</main>
+		</section>
+	<?php }
 
 
 
-<?php get_sidebar();
+get_sidebar();
 
 get_footer();
