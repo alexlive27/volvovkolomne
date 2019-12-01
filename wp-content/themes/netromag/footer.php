611a5bcf7ff12246fa8da4e02c56b7cc848f01c0
alexandr <alexwork2704@yandex.ru>
2019-12-02 02:04:22 +0300

    в шапке  скрипт для вк

--- wp-content/themes/netromag/footer.php
+++ wp-content/themes/netromag/footer.php
@@ -15,6 +15,7 @@
 
 		<footer class="mz-footer" id="footer">
 
+		
 			<!-- footer widgets -->
 			<div class="container footer-inner netro_viget">
 				<div class="row row-gutter">
--- wp-content/themes/netromag/header.php
+++ wp-content/themes/netromag/header.php
@@ -12,6 +12,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php wp_head(); ?>
+
+		<!-- виджет из вк -->
+		<script type="text/javascript" src="https://vk.com/js/api/openapi.js?162"></script>
+
 	</head>
 	<body <?php body_class(); ?>>
 	<div class="no"></div>
