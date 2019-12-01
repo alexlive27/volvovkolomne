2521a297e80f231c3711a27ef999ac10495ce9af
alexandr <alexwork2704@yandex.ru>
2019-12-01 19:29:04 +0300

    адаптивная шапка

--- wp-content/themes/netromag/header.php
+++ wp-content/themes/netromag/header.php
@@ -37,12 +37,12 @@
 		<div class="container">
 			<header class="header" id="header">
 				<div class="container netro_header">
-					<div class="col-md-8 netro_logo_custom"><?php netromag_the_custom_logo(); ?></div>
-					<div class="col-md-5 netro_adress hidden">
+					<div class="col-md-6 netro_logo_custom"><?php netromag_the_custom_logo(); ?></div>
+					<div class="col-md-3 col-xs-6 netro_adress">
 						<p><i class="fa fa-map-marker" aria-hidden="true"></i> г. Коломна ул. Леваневского 36</p>
 						<p><i class="fa fa-map-marker" aria-hidden="true"></i> г. Коломна ул. Добролюбова 17А</p>
 					</div>
-					<div class="col-md-4 netro_tel">
+					<div class="col-md-3 col-xs-6 netro_tel">
 						<a href="tel:79854200340"><i class="fa fa-phone bold" aria-hidden="true"></i> +7 (985) 420-03-40</a>
 						<a href="tel:79854200340"><i class="fa fa-phone bold" aria-hidden="true"></i> +7 (915) 470-73-55</a>
 					</div>
